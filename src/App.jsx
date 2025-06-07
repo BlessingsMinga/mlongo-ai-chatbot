9e50cfa303c116d9fedf8c7006a1821f28f48f01
BlessingsMinga <blessingsminga11@gmail.com>
2025-06-07 07:49:37 +0200

    Display chat messages

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,101 @@
+import { useState } from "react";
 import styles from "./App.module.css";
+import Chat from "./component/Chat/Chat";
 
 function App() {
+
+  const [messages, setMessages] = useState(MESSAGES);
   return (
     <div className={styles.App}>
       <header className={styles.header}>
         <img className={styles.Logo} src="/assets/chat-bot.png" alt="Chat-bot icon" />
         <h2 className={styles.Title}>Mlongo AI Bot</h2>
       </header>
-      <div className={styles.ChatContainer}></div>
+      <div className={styles.ChatContainer}>
+        <Chat messages={messages}/>
+      </div>
     </div>
   );
 }
+
+const MESSAGES = [
+  {
+    role: 'user',
+    content:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ornare augue at vulputate commodo.",
+  },
+
+  {
+    role: 'assistant',
+    content:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ornare augue at vulputate commodo.",
+  },
+  {
+    role: 'user',
+    content:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ornare augue at vulputate commodo.",
+  },
+
+  {
+    role: 'assistant',
+    content:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ornare augue at vulputate commodo.",
+  },
+  {
+    role: 'user',
+    content:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ornare augue at vulputate commodo.",
+  },
+
+  {
+    role: 'assistant',
+    content:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ornare augue at vulputate commodo.",
+  },
+  {
+    role: 'user',
+    content:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ornare augue at vulputate commodo.",
+  },
+
+  {
+    role: 'assistant',
+    content:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ornare augue at vulputate commodo.",
+  },
+  {
+    role: 'user',
+    content:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ornare augue at vulputate commodo.",
+  },
+
+  {
+    role: 'assistant',
+    content:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ornare augue at vulputate commodo.",
+  },
+  {
+    role: 'user',
+    content:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ornare augue at vulputate commodo.",
+  },
+
+  {
+    role: 'assistant',
+    content:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ornare augue at vulputate commodo.",
+  },
+  {
+    role: 'user',
+    content:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ornare augue at vulputate commodo.",
+  },
+
+  {
+    role: 'assistant',
+    content:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ornare augue at vulputate commodo.",
+  },
+];
 
 export default App;
---
+++ src/component/Chat/Chat.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styles from "./Chat.module.css";
+
+const Chat = ({messages}) => {
+  return (
+    <div className={styles.Chat}>
+        {messages.map(({
+            role, content}, index) => 
+        (
+            <div key={index} data-role={role} 
+            className={styles.Message}>{content}</div>
+        ))}
+      
+    </div>
+  )
+}
+
+export default Chat
